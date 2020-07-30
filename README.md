e740860f0f45d1fa79c2a1f626eccc332c329817
kishlay_singh <kishlay_singh@persistent.com>
2020-07-30 11:24:19 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # custom-pet-clinic
+
+custom version of the very famous pet clinic
---
+++ src/test/java/com/sample/custompetclinic/CustomPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.custompetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
