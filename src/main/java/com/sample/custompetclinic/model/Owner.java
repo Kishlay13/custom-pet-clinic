4af14934e5a01665a4e1063b1a519f886a5ac603
kishlay_singh <kishlay_singh@persistent.com>
2020-08-03 19:42:18 +0530

    adding POJOs

---
+++ src/main/java/com/sample/custompetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.sample.custompetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ src/main/java/com/sample/custompetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.sample.custompetclinic.model;
+
+public class Vet extends Person {
+
+}
